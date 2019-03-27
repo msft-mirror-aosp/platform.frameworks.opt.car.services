37aa7edcfb6bef279f2cef58824498028c050f62
Keun-young Park <keunyoung@google.com>
2019-03-27 18:49:54 +0000

    Revert "Unlock user 0 before unlocking other user"
    This reverts commit 5d4c36d62182c612ecc6e6d71478fd6fcbc1ce59.
    
    Reason for revert: Need to handle 1st boot properly
    
    Change-Id: Ibc09b336fb6d296d8c6b143638e5f3de4bd059fc

--- src/com/android/internal/car/CarServiceHelperService.java
+++ src/com/android/internal/car/CarServiceHelperService.java
@@ -16,8 +16,6 @@
 
 package com.android.internal.car;
 
-import android.app.ActivityManager;
-import android.app.IActivityManager;
 import android.app.admin.DevicePolicyManager;
 import android.car.userlib.CarUserManagerHelper;
 import android.content.ComponentName;
@@ -30,10 +28,8 @@ import android.os.IBinder;
 import android.os.Parcel;
 import android.os.RemoteException;
 import android.os.SystemProperties;
-import android.os.Trace;
 import android.os.UserHandle;
 import android.util.Slog;
-import android.util.TimingsTraceLog;
 
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
@@ -51,7 +47,6 @@ import java.util.Map;
 public class CarServiceHelperService extends SystemService {
     // Place holder for user name of the first user created.
     private static final String TAG = "CarServiceHelper";
-    private static final boolean DBG = true;
     private static final String CAR_SERVICE_INTERFACE = "android.car.ICar";
     // These numbers should match with binder call order of
     // packages/services/Car/car-lib/src/android/car/ICar.aidl
@@ -97,10 +92,35 @@ public class CarServiceHelperService extends SystemService {
 
     @Override
     public void onBootPhase(int phase) {
+        // We are starting up another user before PHASE_BOOT_COMPLETE. Services should
+        // still check for User 0's boot complete intent.
         if (phase == SystemService.PHASE_THIRD_PARTY_APPS_CAN_START) {
-            // TODO(b/126199560) Consider doing this earlier in onStart().
-            // Other than onStart, PHASE_THIRD_PARTY_APPS_CAN_START is the earliest timing.
-            setupAndStartUsers();
+            DevicePolicyManager devicePolicyManager =
+                    mContext.getSystemService(DevicePolicyManager.class);
+            if (devicePolicyManager != null && devicePolicyManager.getUserProvisioningState()
+                    != DevicePolicyManager.STATE_USER_UNMANAGED) {
+                return;
+            }
+
+            if (SystemProperties.getBoolean("android.car.systemuser.defaultguest", false)) {
+                // Ensure we switch to the guest user by default
+                // TODO(b/122852856): Localize this string
+                mCarUserManagerHelper.startGuestSession("Guest");
+
+                // Ensure there is an admin user on the device
+                if (mCarUserManagerHelper.getAllAdminUsers().isEmpty()) {
+                    UserInfo admin = mCarUserManagerHelper.createNewAdminUser();
+                }
+            } else {
+                if (mCarUserManagerHelper.getAllUsers().size() == 0) {
+                    // On very first boot, create an admin user and switch to that user.
+                    UserInfo admin = mCarUserManagerHelper.createNewAdminUser();
+                    mCarUserManagerHelper.switchToUserId(admin.id);
+                    mCarUserManagerHelper.setLastActiveUser(admin.id);
+                } else {
+                    mCarUserManagerHelper.switchToUserId(mCarUserManagerHelper.getInitialUser());
+                }
+            }
         }
     }
 
@@ -120,9 +140,6 @@ public class CarServiceHelperService extends SystemService {
     @Override
     public void onUnlockUser(int userHandle) {
         handleUserLockStatusChange(userHandle, true);
-        if (DBG) {
-            Slog.d(TAG, "User" + userHandle + " unlocked");
-        }
     }
 
     @Override
@@ -150,94 +167,6 @@ public class CarServiceHelperService extends SystemService {
             sendSetUserLockStatusBinderCall(userHandle, unlocked);
         }
     }
-
-    private void setupAndStartUsers() {
-        DevicePolicyManager devicePolicyManager =
-                mContext.getSystemService(DevicePolicyManager.class);
-        if (devicePolicyManager != null && devicePolicyManager.getUserProvisioningState()
-                != DevicePolicyManager.STATE_USER_UNMANAGED) {
-            Slog.i(TAG, "DevicePolicyManager active, skip user unlock/switch");
-            return;
-        }
-        // Offloading the whole unlock into separate thread did not help due to single locks
-        // used in AMS / PMS ended up stopping the world with lots of lock contention.
-        // To run these in background, there should be some improvements there.
-        int targetUserId = UserHandle.USER_SYSTEM;
-        if (SystemProperties.getBoolean("android.car.systemuser.defaultguest", false)) {
-            Slog.i(TAG, "Start guest user mode");
-            // Ensure there is an admin user on the device
-            if (mCarUserManagerHelper.getAllAdminUsers().isEmpty()) {
-                Slog.i(TAG, "Create new admin user");
-                UserInfo admin = mCarUserManagerHelper.createNewAdminUser();
-            }
-            // Ensure we switch to the guest user by default
-            // TODO(b/122852856): Localize this string
-            UserInfo guest = mCarUserManagerHelper.createNewOrFindExistingGuest("Guest");
-            if (guest == null) {
-                Slog.e(TAG, "cannot create or find guest user");
-                return;
-            }
-            targetUserId = guest.id;
-        } else {
-            if (mCarUserManagerHelper.getAllUsers().size() == 0) {
-                Slog.i(TAG, "Create new admin user and switch");
-                // On very first boot, create an admin user and switch to that user.
-                UserInfo admin = mCarUserManagerHelper.createNewAdminUser();
-                if (admin == null) {
-                    Slog.e(TAG, "cannot create admin user");
-                    return;
-                }
-                targetUserId = admin.id;
-            } else {
-                Slog.i(TAG, "Switch to default user");
-                targetUserId = mCarUserManagerHelper.getInitialUser();
-            }
-        }
-        // If system user is the only user to unlock, handle it when system completes the boot.
-        if (targetUserId == UserHandle.USER_SYSTEM) {
-            return;
-        }
-        IActivityManager am = ActivityManager.getService();
-        if (am == null) {
-            Slog.wtf(TAG, "cannot get ActivityManagerService");
-            return;
-        }
-        TimingsTraceLog traceLog = new TimingsTraceLog("SystemServerTiming",
-                Trace.TRACE_TAG_ACTIVITY_MANAGER);
-        traceLog.traceBegin("User0Unlock");
-        try {
-            // This is for force changing state into RUNNING_LOCKED. Otherwise unlock does not
-            // update the state and user 0 unlock happens twice.
-            if (!am.startUserInBackground(UserHandle.USER_SYSTEM)) {
-                // cannot start user
-                Slog.w(TAG, "cannot start system user");
-            } else if (!am.unlockUser(UserHandle.USER_SYSTEM, null, null, null)) {
-                // unlocking system user failed. But still continue for other setup.
-                Slog.w(TAG, "cannot unlock system user");
-            } else {
-                // user 0 started and unlocked
-                handleUserLockStatusChange(UserHandle.USER_SYSTEM, true);
-            }
-        } catch (RemoteException e) {
-            // should not happen for local call.
-            Slog.wtf("RemoteException from AMS", e);
-        }
-        traceLog.traceEnd();
-        // Do not unlock here to allow other stuffs done. Unlock will happen
-        // when system completes the boot.
-        // TODO(b/124460424) Unlock earlier?
-        try {
-            if (!am.startUserInForegroundWithListener(targetUserId, null)) {
-                Slog.e(TAG, "cannot start foreground user:" + targetUserId);
-            } else {
-                mCarUserManagerHelper.setLastActiveUser(targetUserId);
-            }
-        } catch (RemoteException e) {
-            // should not happen for local call.
-            Slog.wtf("RemoteException from AMS", e);
-        }
-    }
-
 
     private void notifyAllUnlockedUsers() {
         // only care about unlocked users
@@ -290,7 +219,7 @@ public class CarServiceHelperService extends SystemService {
     }
 
     private void handleCarServiceCrash() {
-        //TODO define recovery behavior
+        //TODO define recovery bahavior
     }
 
     private static native int nativeForceSuspend(int timeoutMs);
