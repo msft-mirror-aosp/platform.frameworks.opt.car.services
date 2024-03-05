8fb418fb56f3f60b0be2244d4d94262efc17444b
Saeid Farivar Asanjan <farivar@google.com>
2024-03-05 23:23:08 +0000

    Add logic to read OEM config for display compat
    Bug: 327009139
    Bug: 291638685
    Test: atest FrameworkOptCarServicesUpdatableTest
    Change-Id: Ifb8e1af33bd1c5ede44de69002625e11d3f3374b

--- updatableServices/src/com/android/server/wm/CarDisplayCompatScaleProviderUpdatableImpl.java
+++ updatableServices/src/com/android/server/wm/CarDisplayCompatScaleProviderUpdatableImpl.java
@@ -19,6 +19,9 @@ import static android.content.pm.PackageManager.GET_CONFIGURATIONS;
 import static android.view.Display.DEFAULT_DISPLAY;
 import static android.view.Display.INVALID_DISPLAY;
 
+import static com.android.server.wm.CarDisplayCompatConfig.ANY_PACKAGE;
+import static com.android.server.wm.CarDisplayCompatConfig.DEFAULT_SCALE;
+
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.SystemApi;
@@ -30,22 +33,29 @@ import android.content.pm.FeatureInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.PackageInfoFlags;
 import android.content.res.CompatScaleWrapper;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
 import android.os.ServiceSpecificException;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.ArrayMap;
 import android.util.AtomicFile;
-import android.util.SparseArray;
-import android.util.Xml;
+import android.util.Pair;
 
 import com.android.car.internal.util.IndentingPrintWriter;
 import com.android.internal.annotations.GuardedBy;
+import com.android.internal.annotations.VisibleForTesting;
 
-import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Implementation of {@link CarDisplayCompatScaleProviderUpdatable}.
@@ -57,20 +67,37 @@ public class CarDisplayCompatScaleProviderUpdatableImpl implements
         CarDisplayCompatScaleProviderUpdatable {
     private static final String TAG = "CarDisplayCompatScaleProvider";
     private static final String FEATURE_DISPLAYCOMPAT = "android.car.displaycompatibility";
+    static final String DISPLAYCOMPAT_SETTINGS_SECURE_KEY =
+            "android.car.displaycompatibility:settings:secure";
     private static final String CONFIG_PATH = "etc/display_compat_config.xml";
-    private static final String NS = null;
+    // {@code android.os.UserHandle.USER_NULL}
+    @VisibleForTesting
+    static final int USER_NULL = -10000;
+    @VisibleForTesting
+    static final float NO_SCALE = -1f;
+    // {@code CarPackageManager#ERROR_CODE_NO_PACKAGE}
+    private static final int ERROR_CODE_NO_PACKAGE = -100;
 
-    private final Object mLock = new Object();
+    @NonNull
+    private Context mContext;
     private final PackageManager mPackageManager;
     private final CarDisplayCompatScaleProviderInterface mCarCompatScaleProviderInterface;
 
-    @GuardedBy("mLock")
+    private final ReentrantReadWriteLock mRWLock = new ReentrantReadWriteLock();
+    @GuardedBy("mRWLock")
     private final ArrayMap<String, Boolean> mRequiresDisplayCompat = new ArrayMap<>();
-
-    private Config mConfig = new Config();
+    @VisibleForTesting
+    final ReentrantReadWriteLock mConfigRWLock = new ReentrantReadWriteLock();
+    @VisibleForTesting
+    @GuardedBy("mConfigRWLock")
+    @NonNull
+    final CarDisplayCompatConfig mConfig = new CarDisplayCompatConfig();
+    @VisibleForTesting
+    ContentObserver mSettingsContentObserver;
 
     public CarDisplayCompatScaleProviderUpdatableImpl(Context context,
             CarDisplayCompatScaleProviderInterface carCompatScaleProviderInterface) {
+        mContext = context;
         mPackageManager = context.getPackageManager();
         mCarCompatScaleProviderInterface = carCompatScaleProviderInterface;
         if (!Flags.displayCompatibility()) {
@@ -83,37 +110,66 @@ public class CarDisplayCompatScaleProviderUpdatableImpl implements
             return;
         }
 
-        try (FileInputStream in = getConfigFile().openRead();) {
-            XmlPullParser parser = Xml.newPullParser();
-            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
-            parser.setInput(in, null);
-            parser.nextTag();
-            mConfig.readConfig(parser);
-        } catch (XmlPullParserException | IOException | SecurityException e) {
-            Slogf.e(TAG, "read config failed", e);
+        if (!updateConfigForUserFromSettings(UserHandle.CURRENT)) {
+            updateCurrentConfigFromDevice();
         }
+
+        // TODO(b/329898692): can we fix the tests so we don't need this?
+        if (mContext.getMainLooper() == null) {
+            // Looper is null during tests.
+            return;
+        }
+
+        mSettingsContentObserver = new ContentObserver(new Handler(mContext.getMainLooper())) {
+            @Override
+            public void onChange(boolean selfChange, Collection<Uri> uris,
+                    int flags, UserHandle user) {
+                super.onChange(selfChange, uris, flags, user);
+                if (selfChange) {
+                    return;
+                }
+                if (getCurrentOrTargetUserId() == user.getIdentifier()) {
+                    updateConfigForUserFromSettings(user);
+                }
+            }
+        };
+        Uri keyUri = Settings.Secure.getUriFor(DISPLAYCOMPAT_SETTINGS_SECURE_KEY);
+        mContext.getContentResolver().registerContentObserverAsUser(keyUri,
+                /*notifyForDescendants*/ true,
+                mSettingsContentObserver, UserHandle.ALL);
     }
 
     @Nullable
     @Override
     public CompatScaleWrapper getCompatScale(@NonNull String packageName, @UserIdInt int userId) {
         if (!Flags.displayCompatibility()) {
-            Slogf.i(TAG, Flags.FLAG_DISPLAY_COMPATIBILITY + " is not enabled");
             return null;
         }
         if (mPackageManager != null
                 && !mPackageManager.hasSystemFeature(FEATURE_DISPLAYCOMPAT)) {
-            Slogf.i(TAG, FEATURE_DISPLAYCOMPAT + " is not available");
             return null;
         }
         try {
             if (requiresDisplayCompat(packageName, userId)) {
-                int display = mCarCompatScaleProviderInterface.getMainDisplayAssignedToUser(userId);
-                if (display == INVALID_DISPLAY) {
-                    display = DEFAULT_DISPLAY;
+                int displayId = mCarCompatScaleProviderInterface
+                        .getMainDisplayAssignedToUser(userId);
+                if (displayId == INVALID_DISPLAY) {
+                    displayId = DEFAULT_DISPLAY;
                 }
-                float scale =  mConfig.get(display, 1.0f);
-                return new CompatScaleWrapper(1.0f, scale);
+                UserHandle user = UserHandle.of(userId);
+                CompatScaleWrapper compatScale = getCompatScaleForPackageAsUser(displayId,
+                        packageName, user);
+                float compatModeScalingFactor = mCarCompatScaleProviderInterface
+                        .getCompatModeScalingFactor(packageName, user);
+                if (compatModeScalingFactor == DEFAULT_SCALE) {
+                    return compatScale;
+                }
+
+                // This shouldn't happen outside of CTS, because CompatModeChanges has higher
+                // priority and will already return a scale.
+                // See {@code com.android.server.wm.CompatModePackage#getCompatScale} for details.
+                return new CompatScaleWrapper(DEFAULT_SCALE,
+                        (1f / compatModeScalingFactor) * compatScale.getDensityScaleFactor());
             }
         } catch (ServiceSpecificException e) {
             return null;
@@ -124,62 +180,66 @@ public class CarDisplayCompatScaleProviderUpdatableImpl implements
     @Override
     public boolean requiresDisplayCompat(@NonNull String packageName, @UserIdInt int userId) {
         boolean result = false;
-        synchronized (mLock) {
+        mRWLock.readLock().lock();
+        try {
             // TODO(b/300642384): need to listen to add/remove of packages from PackageManager so
             // the list doesn't have stale data.
             Boolean res = mRequiresDisplayCompat.get(packageName);
             if (res != null) {
                 return res.booleanValue();
             }
+        } finally {
+            mRWLock.readLock().unlock();
+        }
 
-            try {
-                PackageInfoFlags flags = PackageInfoFlags.of(GET_CONFIGURATIONS);
-                FeatureInfo[] features = mPackageManager.getPackageInfo(packageName, flags)
-                        .reqFeatures;
-                if (features != null) {
-                    for (FeatureInfo feature: features) {
-                        // TODO: get the string from PackageManager
-                        if (FEATURE_DISPLAYCOMPAT.equals(feature.name)) {
-                            Slogf.i(TAG, "detected autoenhance package: " + packageName);
-                            result = true;
-                            break;
-                        }
+        try {
+            PackageInfoFlags flags = PackageInfoFlags.of(GET_CONFIGURATIONS);
+            FeatureInfo[] features = mPackageManager.getPackageInfo(packageName, flags)
+                    .reqFeatures;
+            if (features != null) {
+                for (FeatureInfo feature: features) {
+                    // TODO: get the string from PackageManager
+                    if (FEATURE_DISPLAYCOMPAT.equals(feature.name)) {
+                        result = true;
+                        break;
                     }
                 }
-            } catch (PackageManager.NameNotFoundException e) {
-                Slogf.e(TAG, "Package " + packageName + " not found", e);
-                throw new ServiceSpecificException(
-                        -100 /** {@code CarPackageManager#ERROR_CODE_NO_PACKAGE} */,
-                        e.getMessage());
             }
-
+        } catch (PackageManager.NameNotFoundException e) {
+            Slogf.e(TAG, "Package " + packageName + " not found", e);
+            throw new ServiceSpecificException(
+                    ERROR_CODE_NO_PACKAGE,
+                    e.getMessage());
+        }
+        mRWLock.writeLock().lock();
+        try {
             mRequiresDisplayCompat.put(packageName, result);
+        } finally {
+            mRWLock.writeLock().unlock();
         }
         return result;
     }
 
     /**
-     * Dump {code CarDisplayCompatScaleProviderUpdatableImpl#mRequiresDisplayCompat}
+     * Dump {@code CarDisplayCompatScaleProviderUpdatableImpl#mRequiresDisplayCompat}
+     * and {@code CarDisplayCompatScaleProviderUpdatableImpl#mConfig}
      */
     public void dump(IndentingPrintWriter writer) {
         writer.println(TAG);
         writer.increaseIndent();
         writer.println("DisplayCompat Config:");
         writer.increaseIndent();
-        if (mConfig.size() == 0) {
-            writer.println("Config is empty.");
-        } else {
-            for (int i = 0; i < mConfig.size(); i++) {
-                float scale = mConfig.get(i, -1);
-                if (scale != -1) {
-                    writer.println("display: " + i + " scale: " + scale);
-                }
-            }
+        mConfigRWLock.readLock().lock();
+        try {
+            writer.println(mConfig.dump());
+        } finally {
+            mConfigRWLock.readLock().unlock();
         }
         writer.decreaseIndent();
         writer.println("List of DisplayCompat packages:");
         writer.increaseIndent();
-        synchronized (mLock) {
+        mRWLock.readLock().lock();
+        try {
             if (mRequiresDisplayCompat.size() == 0) {
                 writer.println("No package is enabled.");
             } else {
@@ -189,96 +249,108 @@ public class CarDisplayCompatScaleProviderUpdatableImpl implements
                     }
                 }
             }
+        } finally {
+            mRWLock.readLock().unlock();
         }
         writer.decreaseIndent();
         writer.decreaseIndent();
+    }
+
+    /** Notifies user switching. */
+    public void handleCurrentUserSwitching(UserHandle newUser) {
+        updateConfigForUserFromSettings(newUser);
+    }
+
+    private boolean updateCurrentConfigFromDevice() {
+        mConfigRWLock.writeLock().lock();
+        // read the default config from device if user settings is not available.
+        try (InputStream in = getConfigFile().openRead()) {
+            mConfig.populate(in);
+            mCarCompatScaleProviderInterface.putStringForUser(mContext.getContentResolver(),
+                    DISPLAYCOMPAT_SETTINGS_SECURE_KEY, mConfig.dump(),
+                    UserHandle.CURRENT.getIdentifier());
+            return true;
+        } catch (XmlPullParserException | IOException | SecurityException e) {
+            Slogf.e(TAG, "read config failed from device " + getConfigFile(), e);
+        } finally {
+            mConfigRWLock.writeLock().unlock();
+        }
+        return false;
+    }
+
+    private boolean updateConfigForUserFromSettings(UserHandle user) {
+        // Read the config and populate the in memory cache
+        String configString = mCarCompatScaleProviderInterface.getStringForUser(
+                mContext.getContentResolver(), DISPLAYCOMPAT_SETTINGS_SECURE_KEY,
+                user.getIdentifier());
+        if (configString == null) {
+            return false;
+        }
+        mConfigRWLock.writeLock().lock();
+        try (InputStream in =
+                new ByteArrayInputStream(configString.getBytes())) {
+            mConfig.populate(in);
+            return true;
+        } catch (XmlPullParserException | IOException | SecurityException e) {
+            Slogf.e(TAG, "read config failed from Settings.Secure", e);
+        } finally {
+            mConfigRWLock.writeLock().unlock();
+        }
+        return false;
+    }
+
+    @VisibleForTesting
+    int getCurrentOrTargetUserId() {
+        Pair<Integer, Integer> currentAndTargetUserIds =
+                mCarCompatScaleProviderInterface.getCurrentAndTargetUserIds();
+        int currentUserId = currentAndTargetUserIds.first;
+        int targetUserId = currentAndTargetUserIds.second;
+        int currentOrTargetUserId = targetUserId != USER_NULL
+                ? targetUserId : currentUserId;
+        return currentOrTargetUserId;
+    }
+
+    @Nullable
+    private CompatScaleWrapper getCompatScaleForPackageAsUser(int displayId,
+            @NonNull String packageName, @NonNull UserHandle user) {
+        mConfigRWLock.readLock().lock();
+        try {
+            CarDisplayCompatConfig.Key key =
+                    new CarDisplayCompatConfig.Key(displayId, packageName, user);
+            float scaleFactor = mConfig.getScaleFactor(key, NO_SCALE);
+            if (scaleFactor != NO_SCALE) {
+                return new CompatScaleWrapper(DEFAULT_SCALE, scaleFactor);
+            }
+            // Query the scale factor for all packages for a specific user.
+            key.packageName = ANY_PACKAGE;
+            scaleFactor = mConfig.getScaleFactor(key, NO_SCALE);
+            if (scaleFactor != NO_SCALE) {
+                return new CompatScaleWrapper(DEFAULT_SCALE, scaleFactor);
+            }
+            // Query the scale factor for a specific package across all users.
+            key.packageName = packageName;
+            key.userId = UserHandle.ALL.getIdentifier();
+            scaleFactor = mConfig.getScaleFactor(key, NO_SCALE);
+            if (scaleFactor != NO_SCALE) {
+                return new CompatScaleWrapper(DEFAULT_SCALE, scaleFactor);
+            }
+            // Query the scale factor for a specific display regardless of
+            // user or package name.
+            key.packageName = ANY_PACKAGE;
+            key.userId = UserHandle.ALL.getIdentifier();
+            scaleFactor = mConfig.getScaleFactor(key, NO_SCALE);
+            if (scaleFactor != NO_SCALE) {
+                return new CompatScaleWrapper(DEFAULT_SCALE, scaleFactor);
+            }
+            return null;
+        } finally {
+            mConfigRWLock.readLock().unlock();
+        }
     }
 
     @NonNull
     private static AtomicFile getConfigFile() {
         File configFile = new File(Environment.getProductDirectory(), CONFIG_PATH);
         return new AtomicFile(configFile);
-    }
-
-    private static class Scale {
-        public final int display;
-        public final float scale;
-
-        private Scale(int display, float scale) {
-            this.display = display;
-            this.scale = scale;
-        }
-    }
-
-    private static class Config {
-        private static final String CONFIG = "config";
-        private static final String SCALE = "scale";
-        private static final String DISPLAY = "display";
-
-        private SparseArray<Float> mScales = new SparseArray<>();
-
-        public int size() {
-            return mScales.size();
-        }
-
-        public float get(int index, float defaultValue) {
-            return mScales.get(index, defaultValue);
-        }
-
-        public void readConfig(@NonNull XmlPullParser parser) throws XmlPullParserException,
-                IOException {
-            parser.require(XmlPullParser.START_TAG, NS, CONFIG);
-            while (parser.next() != XmlPullParser.END_TAG) {
-                if (parser.getEventType() != XmlPullParser.START_TAG) {
-                    continue;
-                }
-                String name = parser.getName();
-                if (SCALE.equals(name)) {
-                    Scale scale = readScale(parser);
-                    mScales.put(scale.display, scale.scale);
-                } else {
-                    skip(parser);
-                }
-            }
-        }
-
-        private Scale readScale(@NonNull XmlPullParser parser) throws XmlPullParserException,
-                IOException {
-            parser.require(XmlPullParser.START_TAG, NS, SCALE);
-            int display = DEFAULT_DISPLAY;
-            try {
-                display = Integer.parseInt(parser.getAttributeValue(NS, DISPLAY));
-            } catch (NullPointerException | NumberFormatException e) {
-                Slogf.e(TAG, "parse failed: " + parser.getAttributeValue(NS, DISPLAY), e);
-            }
-            float value = 1f;
-            if (parser.next() == XmlPullParser.TEXT) {
-                try {
-                    value = Float.parseFloat(parser.getText());
-                } catch (NullPointerException | NumberFormatException e) {
-                    Slogf.e(TAG, "parse failed: " + parser.getText(), e);
-                }
-                parser.nextTag();
-            }
-            parser.require(XmlPullParser.END_TAG, NS, SCALE);
-            return new Scale(display, value);
-        }
-
-        private void skip(XmlPullParser parser) throws XmlPullParserException, IOException {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                throw new IllegalStateException();
-            }
-            int depth = 1;
-            while (depth != 0) {
-                switch (parser.next()) {
-                    case XmlPullParser.END_TAG:
-                        depth--;
-                        break;
-                    case XmlPullParser.START_TAG:
-                        depth++;
-                        break;
-                }
-            }
-        }
     }
 }
