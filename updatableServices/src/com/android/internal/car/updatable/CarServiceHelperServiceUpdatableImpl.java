ef67f1218a43a9e57986295c9d9c6c5a5cafb43c
Rachit Jain <jainrachit@google.com>
2025-03-10 01:04:45 +0000

    Update root task information in CarServiceHelperService
    - Add APIs for onRootTaskAppeared and onRootTaskVanished.
    - Redirect activity launch to a root task if root task name is passed in
      the activity launch intent.
    - Use the same intent key for a root task or a private display.
    - Order of launch conditions if condition is not satisfied: root task -> display -> default display.
    
    Bug: 401278097
    Test: atest AutoTaskStackControllerImplTest TaskPanelTest AutoTaskRepositoryTest CarLaunchRedirectActivityInterceptorTest
    Flag: EXEMPT (bug fix)
    Change-Id: I348035cfffddd8a6289d4de11d0815cbb144f86f
    (cherry picked from commit f5db7e38692f35c1c63b68565330411194807b9c)

--- updatableServices/src/com/android/internal/car/updatable/CarServiceHelperServiceUpdatableImpl.java
+++ updatableServices/src/com/android/internal/car/updatable/CarServiceHelperServiceUpdatableImpl.java
@@ -54,10 +54,10 @@ import com.android.server.wm.CarActivityInterceptorUpdatableImpl;
 import com.android.server.wm.CarDisplayCompatActivityInterceptor;
 import com.android.server.wm.CarDisplayCompatScaleProviderInterface;
 import com.android.server.wm.CarDisplayCompatScaleProviderUpdatableImpl;
-import com.android.server.wm.CarLaunchOnPrivateDisplayActivityInterceptor;
 import com.android.server.wm.CarLaunchParamsModifierInterface;
 import com.android.server.wm.CarLaunchParamsModifierUpdatable;
 import com.android.server.wm.CarLaunchParamsModifierUpdatableImpl;
+import com.android.server.wm.CarLaunchRedirectActivityInterceptor;
 
 import java.io.File;
 import java.io.PrintWriter;
@@ -110,6 +110,8 @@ public final class CarServiceHelperServiceUpdatableImpl
 
     private final CarLaunchParamsModifierUpdatableImpl mCarLaunchParamsModifierUpdatable;
     private final CarActivityInterceptorUpdatableImpl mCarActivityInterceptorUpdatable;
+    private final CarLaunchRedirectActivityInterceptor
+            mCarLaunchRedirectActivityInterceptor;
     private CarDisplayCompatScaleProviderUpdatableImpl mCarDisplayCompatScaleProviderUpdatable;
 
     private ExtraDisplayMonitor mExtraDisplayMonitor;
@@ -139,9 +141,11 @@ public final class CarServiceHelperServiceUpdatableImpl
         mCarActivityInterceptorUpdatable.registerInterceptor(0,
                 new CarDisplayCompatActivityInterceptor(context,
                         mCarDisplayCompatScaleProviderUpdatable));
-        // Interceptor for the launch on a private display
+        // Interceptor for redirecting launch on a private display or a root task
+        mCarLaunchRedirectActivityInterceptor =
+                new CarLaunchRedirectActivityInterceptor(context);
         mCarActivityInterceptorUpdatable.registerInterceptor(1,
-                new CarLaunchOnPrivateDisplayActivityInterceptor(context));
+                mCarLaunchRedirectActivityInterceptor);
         // carServiceProxy is Nullable because it is not possible to construct carServiceProxy with
         // "this" object in the previous constructor as CarServiceHelperServiceUpdatableImpl has
         // not been fully constructed.
@@ -358,6 +362,16 @@ public final class CarServiceHelperServiceUpdatableImpl
                 IBinder rootTaskToken) {
             mCarActivityInterceptorUpdatable.setPersistentActivityOnRootTask(activities,
                     rootTaskToken);
+        }
+
+        @Override
+        public void onRootTaskAppeared(String name, IBinder rootTaskToken) {
+            mCarLaunchRedirectActivityInterceptor.onRootTaskAppeared(name, rootTaskToken);
+        }
+
+        @Override
+        public void onRootTaskVanished(String name) {
+            mCarLaunchRedirectActivityInterceptor.onRootTaskVanished(name);
         }
 
         @Override
