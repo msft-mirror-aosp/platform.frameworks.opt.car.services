d4df1c5bfde417a258e909825bec794b75787cc7
Mayank Garg <gargmayank@google.com>
2025-07-18 12:56:27 -0700

    Update CSHS not to route HOME activity
    Flag: EXEMPT bug fix
    
    Bug: 421900011
    Test: Manual checked the SUW doesn't open in the car-wm-shell created task panel.
    Very first time, SUW should open in the HOME root task panel.
    
    Change-Id: I753785327396659856829578ee6b753050f095e4
    (cherry picked from commit 41d5132baf7e981f95b207139177cf75220f72eb)

--- updatableServices/src/com/android/server/wm/CarActivityInterceptorUpdatableImpl.java
+++ updatableServices/src/com/android/server/wm/CarActivityInterceptorUpdatableImpl.java
@@ -16,13 +16,16 @@
 
 package com.android.server.wm;
 
+import static android.view.Display.DEFAULT_DISPLAY;
 import static android.view.Display.INVALID_DISPLAY;
 
 import android.annotation.NonNull;
 import android.annotation.SystemApi;
 import android.app.ActivityOptions;
+import android.app.role.RoleManager;
 import android.car.builtin.util.Slogf;
 import android.content.ComponentName;
+import android.content.Context;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.os.UserHandle;
@@ -61,9 +64,13 @@ public final class CarActivityInterceptorUpdatableImpl implements CarActivityInt
     @NonNull
     private final SparseArray<CarActivityInterceptorUpdatable> mInterceptors = new SparseArray<>();
 
-    public CarActivityInterceptorUpdatableImpl(
+    @NonNull
+    private final RoleManager mRoleManager;
+
+    public CarActivityInterceptorUpdatableImpl(@NonNull Context context,
             @NonNull CarActivityInterceptorInterface builtInInterface) {
         mBuiltIn = builtInInterface;
+        mRoleManager = context.getSystemService(RoleManager.class);
     }
 
     @Override
@@ -73,6 +80,10 @@ public final class CarActivityInterceptorUpdatableImpl implements CarActivityInt
             return null;
         }
         ComponentName componentName = info.getIntent().getComponent();
+
+        if (!canRouteTheActivity(componentName)) {
+            return null;
+        }
 
         synchronized (mLock) {
             int keyIndex = mActivityToRootTaskMap.indexOfKey(componentName);
@@ -108,6 +119,42 @@ public final class CarActivityInterceptorUpdatableImpl implements CarActivityInt
         }
 
         return null;
+    }
+
+    @VisibleForTesting
+    boolean canRouteTheActivity(ComponentName componentName) {
+        if (componentName == null) {
+            Slogf.i(TAG, "componentName is null . Can't route the activity.");
+            return false;
+        }
+
+        // TODO(b/434721819): Explore if we can cache this info with some listener. Also handle
+        // MUMD and distance display case and multi-display cases.
+        List<String> holders = mRoleManager.getRoleHoldersAsUser(RoleManager.ROLE_HOME,
+                UserHandle.of(mBuiltIn.getUserAssignedToDisplay(DEFAULT_DISPLAY)));
+
+
+        if (holders == null || holders.isEmpty()) {
+            Slogf.i(TAG,
+                    "No activity with ROLE_HOME is set. Currently don't have sufficient "
+                            + "information to route the activity.");
+            return false;
+        }
+
+        if (holders.contains(componentName.getPackageName())) {
+            Slogf.i(TAG,
+                    "Request is asking to route the HOME activity. It is not supported. "
+                            + "ComponentName: %s. Current Home activities: %s", componentName,
+                    holders);
+            return false;
+        }
+
+        if (DBG) {
+            Slogf.d(TAG,
+                    "Request is asking to route ComponentName: %s. Current Home activities: %s.",
+                    componentName, holders);
+        }
+        return true;
     }
 
     /**
