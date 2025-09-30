abd5c9dddaf0464e94fd414d64251e04646ab0ca
Yabin Huang <yabinh@google.com>
2025-09-30 12:19:09 -0700

    Ensure activity is launched on the right display on AAOS
    In AAOS multi-user multi-display enviriment,
    when starting an activity for a specific user from Shell
    (e.g., via am start --user 10), the activity was incorrectly
    launching on the focused display (display 2) instead of
    the user's assigned display (display 0).
    
    This was caused by CarLaunchParamsModifier skipping its
    display selection logic and deferring to the default
    TaskLaunchParamsModifier. This happened because for the
    activity launch by the shell, the task and activity objects are null,
    so CarLaunchParamsModifier was unable to determine the
    target userId.
    
    This resulted in the TaskLaunchParamsModifier's fallback
    logic being triggered, which defaults to launching on the
    currently focused display.
    
    This change fixed that by exposing the neccessary info from the
    underlying ActivityStarter.Request.
    
    Fixes: 429003504
    Fixes: 419656704
    Test: atest CtsWindowManagerDeviceActivity:android.server.wm.activity.StartActivityAsUserTests#startActivityAsValidUserWithOptions
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:14c30f528d5fd9d492f6c44d20fafaa519dc2db4
    Merged-In: I23b7b57b7c5d20ab32bffbba121851edd707a125
    Change-Id: I23b7b57b7c5d20ab32bffbba121851edd707a125

--- builtInServices/src/com/android/server/wm/RequestWrapper.java
+++ builtInServices/src/com/android/server/wm/RequestWrapper.java
@@ -18,6 +18,7 @@ package com.android.server.wm;
 
 import android.annotation.Nullable;
 import android.annotation.SystemApi;
+import android.content.ComponentName;
 import android.content.Intent;
 
 
@@ -46,6 +47,23 @@ public final class RequestWrapper {
 
     public Intent getIntent() {
         return mRequest.intent;
+    }
+
+    public int getUserId() {
+        return mRequest.userId;
+    }
+
+    @Nullable
+    public ComponentName getActivityComponentName() {
+        if (mRequest.activityInfo == null) {
+            return null;
+        }
+        String packageName = mRequest.activityInfo.packageName;
+        String className = mRequest.activityInfo.name;
+        if (packageName == null || className == null) {
+            return null;
+        }
+        return new ComponentName(packageName, className);
     }
 
     @Override
